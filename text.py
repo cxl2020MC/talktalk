dfb74337c3b6d1225fcfc0687b8b5c72582d576c
cxl2020MC <andamu2019@163.com>
2022-08-13 09:24:37 +0800

    vscode

--- text.py
+++ text.py
@@ -2,3 +2,8 @@ from itsdangerous import json
 import requests
 
 print(requests.post('https://talktalk-six.vercel.app/api/v1/talktalk', json = {'type': 'load'}).json())
+data =  {'type': 'send', 'data': {
+        'text': '测试测试',
+        'device': 'vscode测试环境'
+    }}
+# print(requests.post('https://talktalk-six.vercel.app/api/v1/talktalk', json = data).json())

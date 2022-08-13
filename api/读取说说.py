97afc9ab68128c3b697b07058cceb2e9cc66e2c6
cxl2020MC <andamu2019@163.com>
2022-08-13 21:31:59 +0800

    vscode

--- api/读取说说.py
+++ api/读取说说.py
@@ -5,19 +5,23 @@ import pymongo
 
 def main(info):
     print('开始读取说说')
+    # 单次查询
     try:
         单次查询数量 = int(info.get('num'))
         if 单次查询数量 == None:
             print('未指定单次查询数量,使用默认数量.')
             单次查询数量 = 15
     except:
+        print('获取单次查询数量失败,使用默认数量.')
         单次查询数量 = 15
+    # 单次跳过
     try:
-        单次跳过数量 = int(info.get('num'))
+        单次跳过数量 = int(info.get('skip'))
         if 单次跳过数量 == None:
             print('未指定单次跳过数量,使用默认数量.')
             单次跳过数量 = 0
     except:
+        print('获取单次跳过数量失败,使用默认数量.')
         单次跳过数量 = 0
     data = list(db.说说.find().limit(单次查询数量).skip(单次跳过数量).sort([("_id", pymongo.DESCENDING)]))
     print('说说数据: {}'.format(data))

c197be5493a773a2a65b6e230ec540ea8543831b
cxl2020MC <andamu2019@163.com>
2022-08-14 21:40:41 +0800

    vscode

--- api/登录.py
+++ api/登录.py
@@ -4,6 +4,8 @@ import os
 
 def main(info):
     密码 = os.getenv('PWD')
+    print('环境变量设置的密码: {}'.format(密码))
+    print('请求中的密码: {}'.format(info.get('pwd')))
     if not 密码:
         raise Exception("未设置PWD环境变量")
     if 密码 == info.get('pwd'):
